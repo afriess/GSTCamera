abf38c618172169a27f820f20eae2f89d88e8faa
Andreas <andi@andifriess.com>
2018-07-02 22:55:29 +0200

    added: the header file gst.h from gstreamer project test: basics in windows 32

--- aravis/samples/Test01/gst.pas
+++ aravis/samples/Test01/gst.pas
@@ -1,3 +1,28 @@
+(*
+ * This is a header translation of
+ * gst.h from gstreamer 1.x
+ * https://gstreamer.freedesktop.org/
+ * The library (.dll/.so) itself is not a part of this package and copyrighted by gstreamer Project
+ *)
+
+(*
+ * Copyright (c) 2018 by Andreas Frieß
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *)
+
 unit gst;
 
 {$mode objfpc}{$H+}
@@ -6,20 +31,36 @@ interface
 
 uses
   Classes, SysUtils, glib2;
+  {GST library
+    @libname: a string with the libraryname,
+              if empty use the default plattform defaultname
+    return true if library correct loaded
+           false if unsuccessfully - do not make a call to the library}
+  function gstlib_initdll(libname:string):boolean;
+  {GST library
+    close the library and free the resources}
+  procedure gstlib_freedll;
+  {GST library
+    return string: string of the version- see gst_version}
+  function gstlib_versionstring:string;
 
-  function gst_initdll(libname:string):boolean;
-  procedure gst_freedll;
-  {GST_API
-   void		gst_version			(guint *major, guint *minor,
-  						 guint *micro, guint *nano);
+  {GST_API gst.h
+   void	gst_init(int *argc, char **argv[]);}
+  procedure gst_init(var argc: gint; var args: pgchar);
+  {GST_API gst.h
+   gboolean gst_init_check(int *argc, char **argv[],GError ** err);}
+  function gst_init_check(var argc: Integer; var args: PAnsiChar; var Error:PGError):gBoolean;
+  {GST_API gst.h
+   void	gst_version(guint *major, guint *minor, guint *micro, guint *nano);
    @major: a number indicating the major version
    @minor: a number indicating the minor version
    @micro: a number indicating the micro version
    @nano:  a number indicating the compiletime version
      Actual releases have 0, GIT versions have 1, prerelease versions have 2-...}
   procedure gst_version(var major, minor, micro, nano: guint);
-
-  function gst_versionstring:string;
+  {GST_API gst.h
+   gchar * gst_version_string(void);}
+  function gst_version_string:Pgchar;
 
 implementation
 
@@ -35,21 +76,29 @@ const
 {$ENDIF}
 
 Type
+  {gst.h}
+  {void	gst_version(guint *major, guint *minor, guint *micro, guint *nano);}
   T_gst_version = procedure (var major, minor, micro, nano: guint);
+  {gchar * gst_version_string(void);}
+  T_gst_version_string = function():pgchar;
+  {void	gst_init (int *argc, char **argv[]);}
+  T_gst_init = procedure (var argc: Integer; var args: PAnsiChar); cdecl;
+  {gboolean gst_init_check(int *argc, char **argv[],GError ** err);}
+  T_gst_init_check = function (var argc: Integer; var args: PAnsiChar; var Error:PGError):gBoolean; cdecl;
 
 var
-  aGST_version : T_gst_version;
-
   libgst_handle: THandle;
+
+  aGST_init                   : T_gst_init;
+  aGST_init_check             : T_gst_init_check;
+  aGST_version                : T_gst_version;
+  aGST_version_string         : T_gst_version_string;
+
 
 function libGST_dll_get_proc_addr(var addr: Pointer; const name: PAnsiChar): Boolean;
 begin
   addr := GetProcedureAddress(libGST_handle, name);
   Result := (addr <> NIL);
-  //if not Result then
-  //begin
-  //  libGST_dynamic_dll_error := 'Procedure "' + name + '" not found!';
-  //end;
 end;
 
 function libGST_dynamic_dll_init(currLib:string):boolean;
@@ -67,7 +116,10 @@ begin
     exit;
   end;
   // allocate the dynamic handles
-  if not libGST_dll_get_proc_addr(pointer(aGST_version),  'gst_version') then inc(ErrCnt);
+  if not libGST_dll_get_proc_addr(pointer(aGST_init), 'gst_init') then inc(ErrCnt);
+  if not libGST_dll_get_proc_addr(pointer(aGST_init_check), 'gst_init_check') then inc(ErrCnt);
+  if not libGST_dll_get_proc_addr(pointer(aGST_version), 'gst_version') then inc(ErrCnt);
+  if not libGST_dll_get_proc_addr(pointer(aGST_version_string), 'gst_version_string') then inc(ErrCnt);
 
   if ErrCnt = 0 then
     Result := true;
@@ -82,38 +134,57 @@ end;
 
 // ------------
 
-function gst_initdll(libname: string): boolean;
+function gstlib_initdll(libname: string): boolean;
 begin
   if (libname = '') then
     libname := libgst_name_1_0;
   Result := libGST_dynamic_dll_init(libname);
 end;
 
-procedure gst_freedll;
+procedure gstlib_freedll;
 begin
   libgst_dynamic_dll_done();
 end;
 
-procedure gst_version(var major, minor, micro, nano: guint);
-begin
-  if (addr(aGST_version) <> nil) then
-    aGST_version(major,minor,micro,nano)
-  else begin
-    major := 0;
-    minor := 0;
-    micro := 0;
-    nano := 0;
-  end;
-
-end;
-
-function gst_versionstring:string;
+function gstlib_versionstring:string;
 var
   major,minor,micro,nano : guint;
 begin
+  major:=0; minor:=0; micro:=0; nano:=0;
   gst_version(major, minor, micro, nano);
   Result:=  IntToStr(major)+'.'+IntToStr(minor)+'.'+IntToStr(micro)+'.'+IntToStr(nano)
 end;
+
+// ------- gst.h ---------
+
+procedure gst_init(var argc: gint; var args: pgchar);
+begin
+  if (addr(aGST_init) <> nil) then
+    aGST_init(argc,args);
+end;
+
+function gst_init_check(var argc: Integer; var args: PAnsiChar;
+  var Error: PGError): gBoolean;
+begin
+  Result := false;
+  if (addr(aGST_init_check) <> nil) then
+    Result := aGST_init_check(argc,args,Error);
+end;
+
+procedure gst_version(var major, minor, micro, nano: guint);
+begin
+  major := 0; minor := 0; micro := 0; nano := 0;
+  if (addr(aGST_version) <> nil) then
+    aGST_version(major,minor,micro,nano)
+end;
+
+function gst_version_string: Pgchar;
+begin
+  Result := PAnsiChar('');
+  if (addr(aGST_version_string) <> nil) then
+    Result := aGST_version_string()
+end;
+
 
 
 initialization
--- aravis/samples/Test01/ugstmain.pas
+++ aravis/samples/Test01/ugstmain.pas
@@ -12,9 +12,13 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
-    Button1: TButton;
+    BuInitLib: TButton;
+    BuReleaseLib: TButton;
+    BuShowVersion: TButton;
     Memo1: TMemo;
-    procedure Button1Click(Sender: TObject);
+    procedure BuInitLibClick(Sender: TObject);
+    procedure BuReleaseLibClick(Sender: TObject);
+    procedure BuShowVersionClick(Sender: TObject);
   private
 
   public
@@ -30,13 +34,65 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.Button1Click(Sender: TObject);
+procedure TForm1.BuInitLibClick(Sender: TObject);
 begin
-  gst_initdll('');
+  if not gstlib_initdll('') then  begin //Q:\gstreamer\1.0\x86\bin\libgstreamer-1.0-0.dll');
+    showmessage('Can not initialize the library');
+    exit;
+  end;
+end;
+
+procedure TForm1.BuReleaseLibClick(Sender: TObject);
+begin
+  gstlib_freedll;
+end;
+
+procedure TForm1.BuShowVersionClick(Sender: TObject);
+var
+  hstr : string;
+  achar : PAnsiChar;
+begin
   Memo1.Clear;
-  Memo1.Append(gst_versionstring);
-  gst_freedll;
+  Memo1.Append(gstlib_versionstring);
+  hstr := '';
+  achar := gst_version_string;
+  hstr  := strpas(achar);
+  Memo1.Append(hstr);
 end;
 
 end.
+
+(* Frfom https://gstreamer.freedesktop.org/documentation/tutorials/basic/hello-world.html
+basic-tutorial-1.c
+
+#include <gst/gst.h>
+
+int main(int argc, char *argv[]) {
+  GstElement *pipeline;
+  GstBus *bus;
+  GstMessage *msg;
+
+  /* Initialize GStreamer */
+  gst_init (&argc, &argv);
+
+  /* Build the pipeline */
+  pipeline = gst_parse_launch ("playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm", NULL);
+
+  /* Start playing */
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+
+  /* Wait until error or EOS */
+  bus = gst_element_get_bus (pipeline);
+  msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+
+  /* Free resources */
+  if (msg != NULL)
+    gst_message_unref (msg);
+  gst_object_unref (bus);
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  gst_object_unref (pipeline);
+  return 0;
+}
+*)
+
 
